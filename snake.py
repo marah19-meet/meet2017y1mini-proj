6ef62b60420576c3ca1f85ba37580e5c4560af82
marah19-meet <marah19@meet.mit.edu>
2017-08-01 17:00:40 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -49,6 +49,11 @@ LEFT=1
 DOWN=2
 RIGHT=3
 direction=UP
+UP_EDGE=250
+DOWN_EDGE=-250
+RIGHT_EDGE=400
+LEFT_EDGE=-400
+
 def up():
     global direction 
     direction=UP 
@@ -58,8 +63,7 @@ def up():
 def down():
     global direction
     direction=DOWN
-    move_snake()
-    print("you pressed the dow key")
+    print("you pressed the down key")
 def right():
     global direction
     direction=RIGHT
@@ -67,44 +71,71 @@ def right():
 def left():
     global direction
     direction=LEFT
-    move_snake()
     print("you pressed the left key")
 
 turtle.onkeypress(up, UP_ARROW)
-turtle.listen()
 turtle.onkeypress(down,DOWN_ARROW)
-turtle.listen()
 turtle.onkeypress(right,RIGHT_ARROW)
-turtle.listen()
 turtle.onkeypress(left,LEFT_ARROW)
 turtle.listen()
+
 def move_snake():
+    global direction
     my_pos = snake.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
     if direction==RIGHT:
         snake.goto(x_pos + SQUARE_SIZE, y_pos)
-        print("You moved right!”)
-              elif direction==LEFT:
+        print("You moved right!")
+    elif direction==LEFT:
         snake.goto(x_pos - SQUARE_SIZE, y_pos)
-        print("You moved left!”)
+        print("You moved left!")
     elif direction==UP:
-        snake.goto(x_pos,y_pos+SQUARE_SIZE)
+        snake.goto(x_pos,y_pos + SQUARE_SIZE)
         print("you moved UP")
     elif direction==DOWN:
         snake.goto(x_pos,y_pos-SQUARE_SIZE)
-my_pos=snake.pos()
-pos_list.append(my_pos)
-new_stamp = snake.stamp()
-stamp_list.append(new_stamp)
-######## SPECIAL PLACE - Remember it for Part 5
-#pop zeroth element in pos_list to get rid of last the last
-#piece of the tail
-old_stamp = stamp_list.pop(0)
-snake.clearstamp(old_stamp)
-pos_list.pop(0)
-    
-    
-        
+        print("you moved down")
+    my_pos=snake.pos()
+    pos_list.append(my_pos)
+    new_stamp = snake.stamp()
+    stamp_list.append(new_stamp)
+    ######## SPECIAL PLACE - Remember it for Part 5
+    #pop zeroth element in pos_list to get rid of last the last
+    #piece of the tail
+    old_stamp = stamp_list.pop(0)
+    snake.clearstamp(old_stamp)
+    pos_list.pop(0)
+
+    new_pos=snake.pos()
+    new_x_pos=new_pos[0]
+    new_y_pos=new_pos[1]
+    if new_x_pos >=RIGHT_EDGE:
+        print("you print the right edge,game over!")
+        quit()
+    elif new_x_pos<= LEFT_EDGE:
+        print("you print the left edge,game over!")
+        quit()
+    elif new_y_pos>=UP_EDGE:
+        print("you print the up edge,game over!")
+        quit()
+    elif new_y_pos<=DOWN_EDGE:
+        print("you print the down edge,game over!")
+        quit()
+    turtle.ontimer(move_snake,TIME_STEP)
+move_snake()
+
+
+turtle.register_shape("trash.gif") 
+food = turtle.clone()
+food.shape("trash.gif")
+food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamps = []
+for this_food_pos in food_pos:
+    x_pos= this_food_pos[0]
+    y_pos= this_food_pos[1]
+    turtle.goto(x_pos,y_pos)
+    stamp.id=food.stamp
+    food_stamps.append(stamp.id)
 
 
